e7b79f5c47ac81c41ff0675eba82523a224096b2
chicodutra <francisco.dutra@escola.pr.gov.br>
2026-07-06 11:32:03 -0300

    op]~

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const botoes = document.querySelectorAll("button")
 console.log(botoes)
+
 botoes.forEach (function(botao){
 let curtida = false;
-botao.addEventListener ("click"), botaoClicado
+botao.addEventListener("click"), botaoClicado
+
     function botaoClicado(){
-     console.log("botao clicado")
-     let texto = botao.querySelector("span")
-     if (curtida == false){
+        console.log("botao clicado")
+         let texto = botao.querySelector("span")
+        if (curtida == false){
         texto.textContent++
-     }
+         }
     }
 })
